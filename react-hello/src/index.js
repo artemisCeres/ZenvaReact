023a38472d0a26e4436f16ec4f905c46dd4bcc93
leila <diana.l.demeter@gmail.com>
2021-12-29 12:09:36 +0200

    created hello world app and initialized shopper app

---
+++ react-hello/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HelloWorld(){
+  return <div>
+      <Hello/> 
+    </div>;
+}
+
+function Hello(){
+//   const name = 'Jane!';
+// return(
+//   <span>Hello {name}</span>
+// ); 
+// let isHello = false;
+// return(
+//   <span>{isHello ? 'Hello' : 'Goodbye'}</span>
+// )
+// let isHello = true;
+// return(
+//   <span>
+//     {isHello && 'Hello'}
+//     {!isHello && 'Goodbye'}
+//     </span>
+// )
+}
+// function World(){
+//   return<span>World</span>;
+// }
+
+// function Hello() {
+//   return React.createElement(
+//     'div',
+//     {}
+//     'Hello World!'
+
+//     )
+// }
+
+ReactDOM.render(
+  <HelloWorld/>, document.querySelector('#root')
+);
